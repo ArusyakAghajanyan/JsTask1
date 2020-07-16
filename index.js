438e21bc6794f88b07777da3428b147b029c662b
Aya30 <ayad_aga@mail.ru>
2020-07-16 11:16:52 +0400

    JsTask1

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// Mi qani tarberakner kan 
+
+ let a = parseInt(prompt("Please enter your first number"));
+ let b = parseInt(prompt("Please enter your second number"));
+ let c = a + b;
+ alert(c);
+
+
+// let a = +prompt("Please enter your first number", '');
+// let b = +prompt("Please enter your second number", '');
+// let c = a + b;
+// alert(c);
+
+
+// let a = Number(prompt("Please enter your first number", ''));
+// let b = Number(prompt("Please enter your second number", ''));
+// let c = a + b;
+// alert(c);
+
+
+// let a = prompt("Please enter your first number", '');
+// let b = prompt("Please enter your second number", '');
+// let c = parseInt(a) + parseInt(b);
+// alert(c);
+
+// let a = prompt("Please enter your first number", '');
+// let b = prompt("Please enter your second number", '');
+// let c = +a + +b; 
+// alert(c);
